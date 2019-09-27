1e9727458060df5892f093910136c117ea8a76ad
aewald79 <55844154+aewald79@users.noreply.github.com>
2019-09-27 11:17:59 -0500

    Update NetmikoConnectloop.py

--- Week1/NetmikoConnectloop.py
+++ Week1/NetmikoConnectloop.py
@@ -1,4 +1,4 @@
-#loading python modules
+#Loading python modules
 from netmiko import ConnectHandler
 from getpass import getpass
 
@@ -7,7 +7,7 @@ cisco_devices = [ "example1", "example2"] #replace with your cisco devices
 ssh_username = input("Username:")
 ssh_pass = getpass()
 
-#Cisco SSH Connection
+#Cisco SSH connection
 for device_host in cisco_devices:
     cisco_netconnect = ConnectHandler(
         host = device_host,
@@ -17,3 +17,5 @@ for device_host in cisco_devices:
         session_log = "Cisco_SSH_Log.txt",
     )
     print (cisco_netconnect.find_prompt())
+    #Disconnects session
+    cisco_netconnect.disconnect()
