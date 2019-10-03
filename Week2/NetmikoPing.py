53a3d15562998b150262ea03c9c1251389ab11c5
Ewald <aewald785@VDI-UBUNTU-01.AD1.D303.ORG>
2019-10-03 13:42:22 -0500

    Week 2

---
+++ Week2/NetmikoPing.py
@@ -0,0 +1,15 @@
+#Loading python modules
+from netmiko import ConnectHandler
+from getpass import getpass
+
+#Cisco SSH connection
+cisco_netconnect = ConnectHandler(
+    host = "example.host",
+    username = input("Username:"),
+    password = getpass(),
+    device_type = "cisco_ios",
+    session_log = "Cisco_SSH_Log.txt",
+    )
+
+ print (cisco_netconnect.send_command("ping"))
+cisco_netconnect.disconnect()
